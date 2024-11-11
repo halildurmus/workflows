e39c10c1f1ca43a0fa6bbd8d371beabd848770cb
Halil Durmus <durmushalil@proton.me>
2024-11-12 00:16:40 +0300

    Create next_version.dart

---
+++ packages/next_version/lib/next_version.dart
@@ -0,0 +1,10 @@
+/// A library that helps you to determine the next version of your package based
+/// on the commits in your repository. package based on the commits in your
+/// repository.
+library;
+
+export 'package:version/version.dart';
+
+export 'src/calculator.dart';
+export 'src/config.dart';
+export 'src/increment.dart';
