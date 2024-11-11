8b6f474c0208aa381b5a08dafb28a021b4170de4
Halil Durmus <durmushalil@proton.me>
2024-11-12 00:23:23 +0300

    Create test.dart

---
+++ packages/hooks/bin/test.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  // Use 'dart' as the default executable if none is provided
+  final executable = args.isNotEmpty ? args.first : 'dart';
+
+  print('🧪 Running tests...');
+
+  // Run the test command.
+  final result = Process.runSync(executable, ['test', ...args]);
+
+  // Output test results.
+  if (result.exitCode == 0) {
+    print('🎉 ✅ All tests passed successfully.');
+  } else {
+    print('🚨 Test failure(s) detected:\n\n${result.stdout}');
+    print('🛑 Please review the test failure(s) above.');
+  }
+
+  // Exit with the same exit code as the test process.
+  exit(result.exitCode);
+}
