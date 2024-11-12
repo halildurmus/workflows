8c5a24684330a2c4815a1486638e9806241c1e78
Halil Durmus <durmushalil@proton.me>
2024-11-12 10:43:15 +0300

    Update main.dart

--- packages/next_version/example/main.dart
+++ packages/next_version/example/main.dart
@@ -1,14 +1,15 @@
 import 'package:next_version/next_version.dart';
 
 void main(List<String> args) {
+  final currentVersion =
+      args.isEmpty ? Version.parse('1.2.3') : Version.parse(args.first);
   final commits = args.isEmpty
       ? const ['feat: add a new feature', 'fix: resolve an issue']
-      : args.join(' ').split(', ');
+      : args.skip(1).join(' ').split(', ');
   print('Commits:');
   for (final c in commits) {
     print(' - $c');
   }
-  final currentVersion = Version.parse('1.2.3');
   final nextVersion = currentVersion.calculateNext(commits);
   print('Next version: $nextVersion');
 }
