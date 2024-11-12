e23c2435a5d592b6cc6f150f679ba159cc309a08
Halil Durmus <durmushalil@proton.me>
2024-11-12 10:49:34 +0300

    Update calculator_test.dart

--- packages/next_version/test/calculator_test.dart
+++ packages/next_version/test/calculator_test.dart
@@ -6,38 +6,9 @@ void main() {
   group('VersionCalculator', () {
     test('applies major increment for breaking change on 1.x.x versions', () {
       final version = Version.parse('1.2.3');
-      const commits = [
-        'feat: add new feature',
-        'fix: bug fix',
-        '''
-feat: breaking API change
-
-BREAKING CHANGE: This is a breaking change.
-'''
-      ];
+      const commits = ['feat!: breaking API change'];
       final nextVersion = version.calculateNext(commits);
       check(nextVersion).equals(Version.parse('2.0.0'));
-    });
-
-    test('applies minor increment for breaking change on 0.x.x versions', () {
-      final version = Version.parse('0.2.3');
-      const commits = [
-        '''
-feat: breaking API change
-
-BREAKING CHANGE: This is a breaking change.
-'''
-      ];
-      final nextVersion = version.calculateNext(commits);
-      check(nextVersion).equals(Version.parse('0.3.0'));
-    });
-
-    test('applies patch increment for non-breaking change on 0.x.x versions',
-        () {
-      final version = Version.parse('0.2.3');
-      const commits = ['feat: add new feature'];
-      final nextVersion = version.calculateNext(commits);
-      check(nextVersion).equals(Version.parse('0.2.4'));
     });
 
     test('applies minor increment for feature commit on 1.x.x versions', () {
@@ -54,6 +25,20 @@ BREAKING CHANGE: This is a breaking change.
       check(nextVersion).equals(Version.parse('1.2.4'));
     });
 
+    test('applies minor increment for breaking change on 0.x.x versions', () {
+      final version = Version.parse('0.2.3');
+      const commits = ['feat!: breaking API change'];
+      final nextVersion = version.calculateNext(commits);
+      check(nextVersion).equals(Version.parse('0.3.0'));
+    });
+
+    test('applies patch increment for feature commit on 0.x.x versions', () {
+      final version = Version.parse('0.2.3');
+      const commits = ['feat: add new feature'];
+      final nextVersion = version.calculateNext(commits);
+      check(nextVersion).equals(Version.parse('0.2.4'));
+    });
+
     test('applies patch increment for fix commit on 0.x.x versions', () {
       final version = Version.parse('0.2.3');
       const commits = ['fix: bug fix'];
@@ -61,14 +46,14 @@ BREAKING CHANGE: This is a breaking change.
       check(nextVersion).equals(Version.parse('0.2.4'));
     });
 
-    test('no increment for unrelated commits', () {
+    test('ignores commits not matching the config', () {
       final version = Version.parse('1.2.3');
-      const commits = ['chore: clean up'];
+      const commits = ['build: update dependencies'];
       final nextVersion = version.calculateNext(commits);
       check(nextVersion).equals(version);
     });
 
-    test('supports custom config to map new types', () {
+    test('supports custom config to map new commit types', () {
       final version = Version.parse('1.2.3');
       const commits = ['docs: update README'];
       final config = VersioningConfig(
@@ -76,13 +61,6 @@ BREAKING CHANGE: This is a breaking change.
       );
       final nextVersion = version.calculateNext(commits, config: config);
       check(nextVersion).equals(Version.parse('1.2.4'));
-    });
-
-    test('ignores commits not matching the config', () {
-      final version = Version.parse('1.2.3');
-      const commits = ['build: update dependencies'];
-      final nextVersion = version.calculateNext(commits);
-      check(nextVersion).equals(version);
     });
 
     test('throws ArgumentError on empty commit list', () {
@@ -100,13 +78,6 @@ BREAKING CHANGE: This is a breaking change.
       check(nextVersion).equals(Version.parse('1.2.3-alpha.2'));
     });
 
-    test('defaults to patch when no other increment type is applicable', () {
-      final version = Version.parse('0.2.3');
-      const commits = ['fix: typo fix'];
-      final nextVersion = version.calculateNext(commits);
-      check(nextVersion).equals(Version.parse('0.2.4'));
-    });
-
     test('applies highest applicable increment from multiple commits', () {
       final version = Version.parse('1.2.3');
       const commits = [
@@ -116,13 +87,6 @@ BREAKING CHANGE: This is a breaking change.
       ];
       final nextVersion = version.calculateNext(commits);
       check(nextVersion).equals(Version.parse('1.3.0'));
-    });
-
-    test('does not increment the version if no significant commit types', () {
-      final version = Version.parse('1.2.3');
-      const commits = ['docs: tweak documentation'];
-      final nextVersion = version.calculateNext(commits);
-      check(nextVersion).equals(Version.parse('1.2.3'));
     });
   });
 
