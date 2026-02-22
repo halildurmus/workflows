028354a50f5c25955af6825be1d047ed3188b8da
Halil Durmus <durmushalil@proton.me>
2026-02-22 15:33:15 +0300

    Remove packages and tools

--- packages/next_version/lib/next_version.dart
+++
@@ -1,9 +0,0 @@
-/// A library that determines the next project version by analyzing conventional
-/// commits.
-library;
-
-export 'package:version/version.dart';
-
-export 'src/calculator.dart';
-export 'src/config.dart';
-export 'src/increment.dart';
